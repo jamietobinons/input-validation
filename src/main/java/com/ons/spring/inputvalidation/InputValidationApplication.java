f9a1e7564c92e9be630ee10af7ec783ee61b8401
Jamie <jamie.tobin@gmail.com>
2018-05-29 13:22:48 +0100

    Initial commit

---
+++ src/main/java/com/ons/spring/inputvalidation/InputValidationApplication.java
@@ -0,0 +1,12 @@
+package com.ons.spring.inputvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputValidationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ons/spring/inputvalidation/InputValidationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ons.spring.inputvalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InputValidationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
